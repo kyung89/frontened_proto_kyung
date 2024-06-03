9ab12b851163b9b5395ef4c2bfd06de771e70881
kyung89 <bamtori1029@gmail.com>
2024-06-03 18:00:24 +0900

    2024-06-03 교육장에서 작업한 데까지

--- src/pages/MainPage/components/CalendarForm/CalendarForm.js
+++ src/pages/MainPage/components/CalendarForm/CalendarForm.js
@@ -1,5 +1,15 @@
-import React from "react";
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 export default function CalendarForm() {
-  return <div>CalendarForm</div>;
+  const [value, onChange] = useState(new Date());
+
+  return (<div>CalendarForm</div>);
+
+  // return (
+  //   <div>
+  //     <Calendar onChange={onChange} value={value} />
+  //   </div>
+  // );
 }
--- src/pages/MainPage/components/IntroduceForm/IntroduceForm.js
+++ src/pages/MainPage/components/IntroduceForm/IntroduceForm.js
@@ -1,5 +1,41 @@
-import React from "react";
+
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import './styles.css';
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 
 export default function IntroduceForm() {
-  return <div>IntroduceForm</div>;
+  return (
+    <>
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide><img src="https://swiperjs.com/demos/images/nature-1.jpg" /></SwiperSlide>
+        <SwiperSlide><img src="https://swiperjs.com/demos/images/nature-2.jpg" /></SwiperSlide>
+        <SwiperSlide><img src="https://swiperjs.com/demos/images/nature-3.jpg" /></SwiperSlide>
+        <SwiperSlide><img src="https://swiperjs.com/demos/images/nature-4.jpg" /></SwiperSlide>
+        <SwiperSlide><img src="https://swiperjs.com/demos/images/nature-5.jpg" /></SwiperSlide>
+      </Swiper>
+    </>
+  );
 }
